0b7f051c9245a303234d18c0b3366342d7ea77ce
Josue Rauda <katerozarack@gmail.com>
2022-11-16 16:26:48 -0600

    Subida de archivo, restricciones de extension

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const { response } = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const subirArchivo = (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     res.status(400).json({
+      errores: {
+        errors: [{ msg: "Los archivos no han sido subidos." }],
+      },
       msg: "Los archivos no han sido subidos.",
     });
     return;
@@ -11,6 +15,9 @@ const subirArchivo = (req, res = response) => {
 
   if (!req.files.archivo) {
     res.status(400).json({
+      errores: {
+        errors: [{ msg: "El archivo no ha sido subido." }],
+      },
       msg: "El archivo no ha sido subido.",
     });
     return;
@@ -19,8 +26,24 @@ const subirArchivo = (req, res = response) => {
   console.log("req.files >>>", req.files); // eslint-disable-line
 
   const { archivo } = req.files;
+  const nombreAbreviado = archivo.name.split(".");
+  const extension = nombreAbreviado[nombreAbreviado.length - 1];
+  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
+  const nuevoNombreArchivo = `${uuidv4()}.${extension}`;
 
-  const uploadPath = path.join(__dirname, "../uploads/", archivo.name);
+  // Validar extensión
+  if (!extensionesValidas.includes(extension)) {
+    res.status(400).json({
+      errores: {
+        errors: [{ msg: `Extensión .${extension} no es valida.` }],
+      },
+      msg: `Extensión .${extension} no es valida.`,
+      extension,
+    });
+    return;
+  }
+
+  const uploadPath = path.join(__dirname, "../uploads/", nuevoNombreArchivo);
 
   archivo.mv(uploadPath, (err) => {
     if (err) {
